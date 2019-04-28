b8b17aa0d717284fbed89421f657e9a3e439ad8c
Glitch (prawinn555-nodejsdemo) <none>
2019-04-28 08:27:49 +0000

    🎄🍡 Checkpoint ./index.html:4136126/9 ./routes/product.js:4136126/180

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 var product_controller = require('../controllers/product');
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 
 // a simple test url to check that all of our files are communicating correctly.
