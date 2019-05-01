6f7c81829f1416e15b70ded4ae57696633f02a41
Glitch (nodejsdemo) <none>
2019-05-01 13:19:12 +0000

    👡🐕 Checkpoint ./controllers/product.js:4136126/1357

--- controllers/product.js
+++ controllers/product.js
@@ -7,15 +7,21 @@ exports.test = function (req, res) {
 };
 
 exports.product_create = function (req, res, next) {
-    console.log('Create product %j', req.body);
-    console.log('Create product param name=%s price=%s', 
+  console.log('Create product %j', req.body);
+  console.log('Create product param name=%s price=%s', 
                 req.body.name, req.body.price);
 
-    var sql    = SqlString.format('INSERT INTO mydata VALUES (?,?)',
+  var sql    = SqlString.format('INSERT INTO mydata VALUES (?,?)',
      [req.body.name, req.body.price]);
-    db.serialize(function() {
-      db.run(sql);
-    });
+  
+  db.run(sql, [], function(err) {
+    if (err) {
+      console.log(err);
+      res.send('error in database');
+    } else { 
+      res.sen('A row has been inserted with rowid ${this.lastID}');
+    }
+  });
 };
 
 exports.product_details = function (req, res, next) {
@@ -23,12 +29,9 @@ exports.product_details = function (req, res, next) {
    var sql    = SqlString.format('SELECT * from mydata where name=?',
       req.params.id);
  
-  
-  db.each(sql, function(err, row) {
-      if ( row ) {
-          res.send(row);
-      }
-    });
+  db.all(sql,[],(err, rows ) => {
+      res.send(rows)
+  });
   
 };
 
@@ -43,19 +46,27 @@ exports.product_list = function (req, res, next) {;
 
 
 exports.product_update = function (req, res, next) {
-    var sql    = SqlString.format('UPDATE mydata VALUES set name=?, time=? where name=?',
+  var sql    = SqlString.format('UPDATE mydata VALUES set name=?, time=? where name=?',
      [req.body.name, req.body.price,req.params.id]);
-    db.serialize(function() {
-      db.run(sql);
-    });
-    res.send('OK');
+  db.run(sql, [], function(err) {
+    if (err) {
+      console.log(err);
+      res.send('error in database');
+    } else { 
+      res.sen('OK');
+    }
+  });
 };
 
 exports.product_delete = function (req, res, next) {
-    var sql    = SqlString.format('DELETE FROM mydata where name=?',
+  var sql    = SqlString.format('DELETE FROM mydata where name=?',
      [req.body.name, req.body.price,req.params.id]);
-    db.serialize(function() {
-      db.run(sql);
-    });
-    res.send('OK');
+  db.run(sql, [], function(err) {
+    if (err) {
+      console.log(err);
+      res.send('error in database');
+    } else { 
+      res.sen('OK');
+    }
+  });;
 };
